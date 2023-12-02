91e99b30d4af9262708b50bcab81c4df06311766
Sasha Alimov <aalimov@wiley.com>
2023-12-01 19:46:48 -0500

    fix

--- src/app.py
+++ src/app.py
@@ -35,7 +35,7 @@ data_json = []
 for x in range(1, 51):
     file_indexes_str = str(x).zfill(3)
 
-    file_name = f"src/static/png/result3000/3_{file_indexes_str}-ascii-art.txt"
+    file_name = f"src/static/png/result/3_{file_indexes_str}-ascii-art.txt"
     with open(file_name, "r") as file:
         data_json.append(file.read())
 
