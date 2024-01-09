7c943e28325458f2f6242141f7e461098e247968
Sasha Alimov <aalimov@wiley.com>
2024-01-09 16:11:50 -0500

    try2

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 # import multiprocessing
 
-# bind = "0.0.0.0:5000"  # Bind to all available network interfaces
+bind = "0.0.0.0:5000"  # Bind to all available network interfaces
 # workers = multiprocessing.cpu_count() * 2 + 1  # Use formula to determine the number of workers
 # worker_class = "gthread"  # Use threads for concurrency
 # threads = multiprocessing.cpu_count() * 2  # Use twice as many threads as there are workers
